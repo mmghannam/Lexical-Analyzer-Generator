71aebc4ec395c338f8477213bc61d72f0aaf7e41
Amr Fathy <amruafathy@gmail.com>
2017-03-30 19:19:06 +0200

    Generate transition table

--- src/dfa.py
+++ src/dfa.py
@@ -1,25 +1,54 @@
+from collections import namedtuple, OrderedDict
 from networkx import *
-from .dfa_state import *
 import matplotlib.pyplot as plt
+import string
+from prettytable import PrettyTable
+
+DFAState = namedtuple('DFAState', ['index', 'NFAStates', 'acceptance'])
 
 
 class DFA:
     def __init__(self, nfa):
         self.start_node = None
         self.end_node = None
-        self.graph = DiGraph()
+        self.node_index = 0
+        self.transition_table = {}
 
+        self.init_input_list()
         self.generate_dfa(nfa)
 
     def generate_dfa(self, nfa):
         self.start_node = nfa.start_node
 
-        states = set()
+        self.states = list()
+        worklist = list()
 
-        start_state = DFAState(nfa.get_epsilon_closures(nfa.start_node))
-        states.add(start_state)
+        start_state = DFAState(self.get_node_index(), nfa.get_epsilon_closures(nfa.start_node))
+        # print(start_state)
 
-        print(start_state)
+        self.states.append(start_state)
+        worklist.append(start_state)
+
+        while worklist:
+            current_state = worklist.pop()
+            self.transition_table[current_state.index] = OrderedDict()
+
+            for input_literal in self.input_list:
+                new_nfa_states = nfa.get_epsilon_closures(nfa.move(current_state.NFAStates, input_literal))
+
+                if not new_nfa_states:  # rejection
+                    new_dfa_state = DFAState(-1, set())
+                else:
+                    new_state_index = self.get_node_index()
+                    new_dfa_state = DFAState(new_state_index, new_nfa_states)
+
+                self.transition_table[current_state.index][input_literal] = new_dfa_state.index
+
+                if new_dfa_state not in self.states:
+                    self.states.append(new_dfa_state)
+                    worklist.append(new_dfa_state)
+
+        self.print_transition_table()
 
     def is_letter(self, char):
         return (ord('a') <= ord(char) <= ord('z')) or \
@@ -28,3 +57,41 @@ class DFA:
     def is_digit(self, num):
         return (ord('0') <= ord(num) <= ord('9')) if isinstance(num, str) \
             else (0 <= num <= 9)
+
+    def get_node_index(self):
+        """
+        This method keeps track of the index of nodes and returns a different index each time by incrementing the static
+        class variable __cni
+        """
+        self.node_index += 1
+        return self.node_index - 1
+
+    def print_transition_table(self):
+
+        print('=' * 100)
+
+        table = PrettyTable(['State'] + [x for x in self.input_list])
+
+        for index in self.transition_table:
+            row = self.transition_table[index]
+
+            state = str(index) + ': ' + (str(self.states[index].NFAStates)) \
+                if self.states[index].NFAStates else 'Rejection'
+
+            table.add_row([state] + [row[x] for x in row])
+
+        print(table)
+
+    def init_input_list(self):
+        letters = list(string.ascii_letters)
+        digits = list(string.digits)
+        operators = [  # '==', '!=', '>', '>=', '<', '<=',  # relational operators
+            '=',  # assignment operator
+            '+', '-',  # add operators
+            '*', '/',  # multiply operators
+            ';', ',', '(', ')', '{', '}'  # reserved operators
+        ]
+
+        # self.input_list = letters + digits + operators
+        self.input_list = 'abcd'
+
--- src/main.py
+++ src/main.py
@@ -2,12 +2,12 @@ from src.nfa import *
 from src.dfa import *
 from src.tokenizer import *
 
-nfa = NFA.from_tokens([Token('1-3', 'bibo')])
+nfa = NFA.from_tokens([Token('(a|B)*a', 'bibo')])
 print('start: ', nfa.start_node)
 print('end: ', nfa.end_node)
 
 # print(nfa.move(1, {1, 2}))
 
-dfa = DFA(nfa)
+# dfa = DFA(nfa)
 
 nfa.draw()
